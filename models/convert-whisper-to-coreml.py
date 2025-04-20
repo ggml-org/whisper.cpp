350f1d9d4552b6c74d6b5d7381ff9483a705789b
Daniel Bevenius <daniel.bevenius@gmail.com>
2025-04-20 15:47:32 +0200

    coreml : set convert_to="mlprogram" in convert
    This commit updates the conversion process for Whisper models to use the
    "mlprogram" format instead of "neuralnetwork".
    
    The motivation for this change is that when using the "neuralnetwork"
    format the underlying model produced is based on protobuf and my
    understanding is that there are limitations to this format, such as
    sizes of strings and the complexity of the model.
    
    Currently when trying to convert larger models such as large-v3 the
    conversion fails but succeeds for smaller models.
    
    The "mlprogram" format is a more recent addition to CoreML and is
    designed to be more flexible and powerful, allowing for more complex
    models and larger data types. This seems to work for larger and smaller
    models alike and unless I'm there are considerations that I'm not aware
    of I think this is what we should be using moving forward.

--- models/convert-whisper-to-coreml.py
+++ models/convert-whisper-to-coreml.py
@@ -254,11 +254,10 @@ def convert_encoder(hparams, model, quantize=False):
 
     model = ct.convert(
         traced_model,
-        convert_to="neuralnetwork",
+        convert_to="mlprogram",
         inputs=[ct.TensorType(name="logmel_data", shape=input_shape)],
         outputs=[ct.TensorType(name="output")],
         compute_units=ct.ComputeUnit.ALL,
-        skip_model_load=True,
     )
 
     if quantize:
@@ -279,12 +278,11 @@ def convert_decoder(hparams, model, quantize=False):
 
     model = ct.convert(
         traced_model,
-        convert_to="neuralnetwork",
+        convert_to="mlprogram",
         inputs=[
             ct.TensorType(name="token_data", shape=tokens_shape, dtype=int),
             ct.TensorType(name="audio_data", shape=audio_shape)
         ],
-        skip_model_load=True,
     )
 
     if quantize:
