d6c7a10da00c5c0c30a3691aadefe6bdf2af2544
peardox <simon@peardox.com>
2025-04-30 22:49:26 +0100

    Oops - accidental commit - fixup

--- examples/bench/bench.cpp
+++ examples/bench/bench.cpp
@@ -66,11 +66,8 @@ static int whisper_bench_full(const whisper_params & params) {
     // whisper init
 
     #ifdef BINDINGS_FLAT
-    // whisper_flat_backend_load_all();
-    ggml_backend_try_load_best("rpc", nullptr);
-    ggml_backend_try_load_best("blas", nullptr);
-    ggml_backend_try_load_best("cpu", nullptr);
-#endif
+    whisper_flat_backend_load_all();
+    #endif
 
     struct whisper_context_params cparams = whisper_context_default_params();
 
