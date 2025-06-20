fc29e841b6ea8a85b25ca6ccfb8b301b2be84e17
glaszig <mail@glasz.org>
2025-06-19 22:04:47 -0300

    run predictions on background queues

--- src/coreml/MLModel+Compat.m
+++ src/coreml/MLModel+Compat.m
@@ -7,20 +7,26 @@
 
 - (void) predictionFromFeatures:(id<MLFeatureProvider>) input
               completionHandler:(void (^)(id<MLFeatureProvider> output, NSError * error)) completionHandler {
-    [NSOperationQueue.mainQueue addOperationWithBlock:^{
+    [NSOperationQueue.new addOperationWithBlock:^{
         NSError *error = nil;
         id<MLFeatureProvider> prediction = [self predictionFromFeatures:input error:&error];
-        completionHandler(prediction, error);
+
+        [NSOperationQueue.mainQueue addOperationWithBlock:^{
+            completionHandler(prediction, error);
+        }];
     }];
 }
 
 - (void) predictionFromFeatures:(id<MLFeatureProvider>) input
                         options:(MLPredictionOptions *) options
               completionHandler:(void (^)(id<MLFeatureProvider> output, NSError * error)) completionHandler {
-    [NSOperationQueue.mainQueue addOperationWithBlock:^{
+    [NSOperationQueue.new addOperationWithBlock:^{
         NSError *error = nil;
         id<MLFeatureProvider> prediction = [self predictionFromFeatures:input options:options error:&error];
-        completionHandler(prediction, error);
+
+        [NSOperationQueue.mainQueue addOperationWithBlock:^{
+            completionHandler(prediction, error);
+        }];
     }];
 }
 
