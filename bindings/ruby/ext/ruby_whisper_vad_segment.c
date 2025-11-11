006bc602aa52191fa0f938f561459e7802361c32
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2025-11-11 19:03:47 +0900

    Define Whisper::VAD::Segment#deconstruct_keys

--- bindings/ruby/ext/ruby_whisper_vad_segment.c
+++ bindings/ruby/ext/ruby_whisper_vad_segment.c
@@ -1,9 +1,15 @@
 #include <ruby.h>
 #include "ruby_whisper.h"
 
+#define N_KEY_NAMES 2
+
 extern VALUE cVADSegment;
 
 extern const rb_data_type_t ruby_whisper_vad_segments_type;
+
+static VALUE sym_start_time;
+static VALUE sym_end_time;
+static VALUE key_names;
 
 static void
 rb_whisper_vad_segment_mark(void *p)
@@ -33,7 +39,7 @@ static const rb_data_type_t ruby_whisper_vad_segment_type = {
     0
 };
 
-VALUE
+static VALUE
 ruby_whisper_vad_segment_s_allocate(VALUE klass)
 {
   ruby_whisper_vad_segment *rwvs;
@@ -51,7 +57,7 @@ rb_whisper_vad_segment_s_new(VALUE segments, int index)
   return segment;
 }
 
-VALUE
+static VALUE
 ruby_whisper_vad_segment_get_start_time(VALUE self)
 {
   ruby_whisper_vad_segment *rwvs;
@@ -64,7 +70,7 @@ ruby_whisper_vad_segment_get_start_time(VALUE self)
   return DBL2NUM(t0 * 10);
 }
 
-VALUE
+static VALUE
 ruby_whisper_vad_segment_get_end_time(VALUE self)
 {
   ruby_whisper_vad_segment *rwvs;
@@ -77,11 +83,56 @@ ruby_whisper_vad_segment_get_end_time(VALUE self)
   return DBL2NUM(t1 * 10);
 }
 
+static VALUE
+ruby_whisper_vad_segment_deconstruct_keys(VALUE self, VALUE keys)
+{
+  ruby_whisper_vad_segment *rwvs;
+  ruby_whisper_vad_segments *rwvss;
+  VALUE hash, key;
+  long n_keys;
+  int i;
+
+  TypedData_Get_Struct(self, ruby_whisper_vad_segment, &ruby_whisper_vad_segment_type, rwvs);
+  TypedData_Get_Struct(rwvs->segments, ruby_whisper_vad_segments, &ruby_whisper_vad_segments_type, rwvss);
+
+  hash = rb_hash_new();
+  if (NIL_P(keys)) {
+    keys = key_names;
+    n_keys = N_KEY_NAMES;
+  } else {
+    n_keys = RARRAY_LEN(keys);
+    if (n_keys > N_KEY_NAMES) {
+      return hash;
+    }
+  }
+  for (i = 0; i < n_keys; i++) {
+    key = rb_ary_entry(keys, i);
+    if (key == sym_start_time) {
+      rb_hash_aset(hash, key, ruby_whisper_vad_segment_get_start_time(self));
+    }
+    if (key == sym_end_time) {
+      rb_hash_aset(hash, key, ruby_whisper_vad_segment_get_end_time(self));
+    }
+  }
+
+  return hash;
+}
+
 void
 init_ruby_whisper_vad_segment(VALUE *mVAD)
 {
   cVADSegment = rb_define_class_under(*mVAD, "Segment", rb_cObject);
+
+  sym_start_time = ID2SYM(rb_intern("start_time"));
+  sym_end_time = ID2SYM(rb_intern("end_time"));
+  key_names = rb_ary_new3(
+    N_KEY_NAMES,
+    sym_start_time,
+    sym_end_time
+  );
+
   rb_define_alloc_func(cVADSegment, ruby_whisper_vad_segment_s_allocate);
   rb_define_method(cVADSegment, "start_time", ruby_whisper_vad_segment_get_start_time, 0);
   rb_define_method(cVADSegment, "end_time", ruby_whisper_vad_segment_get_end_time, 0);
+  rb_define_method(cVADSegment, "deconstruct_keys", ruby_whisper_vad_segment_deconstruct_keys, 1);
 }
