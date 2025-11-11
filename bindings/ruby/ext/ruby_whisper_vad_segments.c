b938ea6d1fdee8853a98cb53c28ca3df83aa9ce8
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2025-11-11 18:44:19 +0900

    Define Whisper::VAD::Segments#each

--- bindings/ruby/ext/ruby_whisper_vad_segments.c
+++ bindings/ruby/ext/ruby_whisper_vad_segments.c
@@ -1,8 +1,12 @@
 #include <ruby.h>
 #include "ruby_whisper.h"
 
-VALUE cVADSegments;
+extern ID id___method__;
+extern ID id_to_enum;
+
 extern VALUE cVADSegments;
+
+extern VALUE rb_whisper_vad_segment_s_new(VALUE segments, int index);
 
 static size_t
 ruby_whisper_vad_segments_memsize(const void *p)
@@ -45,9 +49,32 @@ ruby_whisper_vad_segments_s_allocate(VALUE klass)
   return obj;
 }
 
+static VALUE
+ruby_whisper_vad_segments_each(VALUE self)
+{
+  ruby_whisper_vad_segments *rwvss;
+  VALUE method_name;
+  int n_segments, i;
+
+  if (!rb_block_given_p()) {
+    method_name = rb_funcall(self, id___method__, 0);
+    return rb_funcall(self, id_to_enum, 1, method_name);
+  }
+
+  TypedData_Get_Struct(self, ruby_whisper_vad_segments, &ruby_whisper_vad_segments_type, rwvss);
+  n_segments = whisper_vad_segments_n_segments(rwvss->segments);
+  for (i = 0; i < n_segments; ++i) {
+    rb_yield(rb_whisper_vad_segment_s_new(self, i));
+  }
+
+  return self;
+}
+
 void
 init_ruby_whisper_vad_segments(VALUE *mVAD)
 {
   cVADSegments = rb_define_class_under(*mVAD, "Segments", rb_cObject);
   rb_define_alloc_func(cVADSegments, ruby_whisper_vad_segments_s_allocate);
+  rb_define_method(cVADSegments, "each", ruby_whisper_vad_segments_each, 0);
+  rb_include_module(cVADSegments, rb_path2class("Enumerable"));
 }
