de57dd9c12d8034d364339193032366a51be4c5e
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2025-11-11 19:04:05 +0900

    Add tests for Whisper::VAD family

--- bindings/ruby/test/test_vad_context.rb
+++ bindings/ruby/test/test_vad_context.rb
@@ -1,8 +1,41 @@
 require_relative "helper"
 
-class TestVADContext < Test::Unit::TestCase
+class TestVADContext < TestBase
   def test_initialize
     context = Whisper::VAD::Context.new("silero-v5.1.2")
     assert_instance_of Whisper::VAD::Context, context
+  end
+
+  def test_detect
+    context = Whisper::VAD::Context.new("silero-v5.1.2")
+    segments = context.detect(AUDIO, Whisper::VAD::Params.new)
+    assert_instance_of Whisper::VAD::Segments, segments
+
+    i = 0
+    segments.each do |segment|
+      i += 1
+      assert_instance_of Whisper::VAD::Segment, segment
+    end
+    assert i > 0
+
+    segments.each_with_index do |segment, index|
+      assert_instance_of Integer, index
+    end
+
+    assert_instance_of Enumerator, segments.each
+
+    segment = segments.each.first
+    assert_instance_of Float, segment.start_time
+    assert_instance_of Float, segment.end_time
+
+    segment => {start_time:, end_time:}
+    assert_equal segment.start_time, start_time
+    assert_equal segment.end_time, end_time
+  end
+
+  def test_invalid_model_type
+    assert_raise TypeError do
+      Whisper::VAD::Context.new(Object.new)
+    end
   end
 end
