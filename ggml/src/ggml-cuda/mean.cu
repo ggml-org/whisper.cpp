334e68b202904a407d639a1c37db29d8bb3754d5
Jamaika1 <lukaszcz18@wp.pl>
2026-01-06 11:05:20 +0100

    Change USE_CUDA_GRAPH
    ```
    mean.cu:36:53: warning: suggest parentheses around '&&' within '||' [-Wparentheses]
       36 |              ((ctx.cuda_graph->instance == nullptr) && (iscapturing == cudaStreamCaptureStatusNone) ||
          |               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    mean.cu:41:50: warning: suggest parentheses around '&&' within '||' [-Wparentheses]
       41 |          !((ctx.cuda_graph->instance == nullptr) && (iscapturing == cudaStreamCaptureStatusNone) ||
          |            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    mean.cu:32:22: warning: suggest parentheses around '&&' within '||' [-Wparentheses]
       32 |     if ((nrows == 1) &&
          |         ~~~~~~~~~~~~~^~
       33 | #ifdef USE_CUDA_GRAPH
          | ~~~~~~~~~~~~~~~~~~~~~
       34 |             // CUDA_GRAPHS_DISABLED
          |             ~~~~~~~~~~~~~~~~~~~~~~~
       35 |             ((ncols > 65536) &&
          |             ~~~~~~~~~~~~~~~~~~~
       36 |              ((ctx.cuda_graph->instance == nullptr) && (iscapturing == cudaStreamCaptureStatusNone) ||
          |              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
       37 |               ctx.cuda_graph->disable_due_to_gpu_arch || ctx.cuda_graph->disable_due_to_too_many_updates ||
          |               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
       38 |               ctx.cuda_graph->disable_due_to_failed_graph_capture)) ||
          |               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    ```

--- ggml/src/ggml-cuda/mean.cu
+++ ggml/src/ggml-cuda/mean.cu
@@ -28,9 +28,7 @@ void ggml_cuda_op_mean(ggml_backend_cuda_context & ctx, ggml_tensor * dst) {
 #ifdef USE_CUDA_GRAPH
     cudaStreamCaptureStatus iscapturing;
     CUDA_CHECK(cudaStreamIsCapturing(stream, &iscapturing));
-#endif // USE_CUDA_GRAPH
     if ((nrows == 1) &&
-#ifdef USE_CUDA_GRAPH
             // CUDA_GRAPHS_DISABLED
             ((ncols > 65536) &&
              ((ctx.cuda_graph->instance == nullptr) && (iscapturing == cudaStreamCaptureStatusNone) ||
@@ -42,6 +40,7 @@ void ggml_cuda_op_mean(ggml_backend_cuda_context & ctx, ggml_tensor * dst) {
            ctx.cuda_graph->disable_due_to_gpu_arch || ctx.cuda_graph->disable_due_to_too_many_updates ||
            ctx.cuda_graph->disable_due_to_failed_graph_capture))) {
 #else
+    if ((nrows == 1) &&
         (ncols > 65536)) {
 #endif // USE_CUDA_GRAPH
         // Single row - use device-wide reduction
