4b573c99c1fc370d8b32dada32ec040022e7f557
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2026-01-28 12:20:28 +0900

    Make Whisper::Context#transcribe and Whisper::VAD::Context#detect accept Pathname

--- bindings/ruby/ext/ruby_whisper_vad_context_detect.cpp
+++ bindings/ruby/ext/ruby_whisper_vad_context_detect.cpp
@@ -8,6 +8,8 @@
 extern "C" {
 #endif
 
+extern ID id_to_path;
+
 extern VALUE cVADSegments;
 
 extern const rb_data_type_t ruby_whisper_vad_context_type;
@@ -28,6 +30,9 @@ ruby_whisper_vad_detect(VALUE self, VALUE file_path, VALUE params) {
   GetVADContext(self, rwvc);
   TypedData_Get_Struct(params, ruby_whisper_vad_params, &ruby_whisper_vad_params_type, rwvp);
 
+  if (rb_respond_to(file_path, id_to_path)) {
+    cpp_file_path = rb_funcall(file_path, id_to_path, 0);
+  }
   cpp_file_path = StringValueCStr(file_path);
 
   if (!read_audio_data(cpp_file_path, pcmf32, pcmf32s, false)) {
