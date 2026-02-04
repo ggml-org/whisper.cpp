1432fdb19189c0c4ee916f29b93e7275143108cf
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2026-02-04 16:58:11 +0900

    Add test for Context::Params.new with keyword args

--- bindings/ruby/test/test_context_params.rb
+++ bindings/ruby/test/test_context_params.rb
@@ -1,6 +1,15 @@
 require_relative "helper"
 
 class TestContextParams < TestBase
+  PARAM_NAMES = [
+    :use_gpu,
+    :flash_attn,
+    :gpu_device,
+    :dtw_token_timestamps,
+    :dtw_aheads_preset,
+    :dtw_n_top
+  ]
+
   def test_new
     params = Whisper::Context::Params.new
     assert_instance_of Whisper::Context::Params, params
@@ -34,5 +43,40 @@ class TestContextParams < TestBase
     assert_equal 6, params.dtw_n_top
     params.dtw_n_top = nil
     assert_nil params.dtw_n_top
+  end
+
+  def test_new_with_kw_args
+    params = Whisper::Context::Params.new(use_gpu: false)
+    assert_false params.use_gpu
+  end
+
+  def test_new_with_kw_wargs_non_existent
+    assert_raise ArgumentError do
+      Whisper::Context::Params.new(non_existent: "value")
+    end
+  end
+
+  data(PARAM_NAMES.collect {|param| [param, param]}.to_h)
+  def test_new_with_kw_args_default_values(param)
+    default_params = Whisper::Context::Params.new
+    default_value = default_params.send(param)
+    value = if param == :dtw_n_top
+              6
+            else
+              case default_value
+              in true | false
+                !default_value
+              in Integer
+                default_value + 1
+              end
+            end
+    params = Whisper::Context::Params.new(param => value)
+    assert_equal value, params.send(param)
+
+    PARAM_NAMES.reject {|name| name == param}.each do |name|
+      expected = default_params.send(name)
+      actual = params.send(name)
+      assert_equal expected, actual
+    end
   end
 end
