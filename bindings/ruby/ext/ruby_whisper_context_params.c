f2d65cadf755366c00f42c02b43f2442a1d4b3a2
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2026-02-04 16:58:00 +0900

    Make Context::Params.new accept keyword args

--- bindings/ruby/ext/ruby_whisper_context_params.c
+++ bindings/ruby/ext/ruby_whisper_context_params.c
@@ -1,5 +1,7 @@
 #include "ruby_whisper.h"
 
+#define NUM_PARAMS 6
+
 #define DEF_BOOLEAN_ATTR_METHOD(name) \
 static VALUE \
 ruby_whisper_context_params_get_ ## name(VALUE self) { \
@@ -30,7 +32,21 @@ ruby_whisper_context_params_set_ ## name(VALUE self, VALUE value) { \
   return value; \
 }
 
+#define DEFINE_PARAM(param_name, nth) \
+  id_ ## param_name = rb_intern(#param_name); \
+  param_names[nth] = id_ ## param_name; \
+  rb_define_method(cContextParams, #param_name, ruby_whisper_context_params_get_ ## param_name, 0); \
+  rb_define_method(cContextParams, #param_name "=", ruby_whisper_context_params_set_ ## param_name, 1);
+
 VALUE cContextParams;
+
+static ID param_names[NUM_PARAMS];
+static ID id_use_gpu;
+static ID id_flash_attn;
+static ID id_gpu_device;
+static ID id_dtw_token_timestamps;
+static ID id_dtw_aheads_preset;
+static ID id_dtw_n_top;
 
 static size_t
 ruby_whisper_context_params_memsize(const void *p)
@@ -96,15 +112,48 @@ ruby_whisper_context_params_set_dtw_n_top(VALUE self, VALUE value) {
   return value;
 }
 
+#define SET_PARAM_IF_SAME(param_name) \
+  if (id == id_ ## param_name) { \
+    ruby_whisper_context_params_set_ ## param_name(self, value); \
+    continue; \
+  }
+
 static VALUE
-ruby_whisper_context_params_initialize(VALUE self)
+ruby_whisper_context_params_initialize(int argc, VALUE *argv, VALUE self)
 {
   ruby_whisper_context_params *rwcp;
   TypedData_Get_Struct(self, ruby_whisper_context_params, &ruby_whisper_context_params_type, rwcp);
   *rwcp->params = whisper_context_default_params();
 
+  VALUE kw_hash;
+  rb_scan_args_kw(RB_SCAN_ARGS_KEYWORDS, argc, argv, ":", &kw_hash);
+  if (NIL_P(kw_hash)) {
+    return Qnil;
+  }
+
+  VALUE values[NUM_PARAMS] = {Qundef};
+  rb_get_kwargs(kw_hash, param_names, 0, NUM_PARAMS, values);
+
+  ID id;
+  VALUE value;
+  for (int i = 0; i < NUM_PARAMS; i++) {
+    id = param_names[i];
+    value = values[i];
+    if (value == Qundef) {
+      continue;
+    }
+    SET_PARAM_IF_SAME(use_gpu)
+    SET_PARAM_IF_SAME(flash_attn)
+    SET_PARAM_IF_SAME(gpu_device)
+    SET_PARAM_IF_SAME(dtw_token_timestamps)
+    SET_PARAM_IF_SAME(dtw_aheads_preset)
+    SET_PARAM_IF_SAME(dtw_n_top)
+  }
+
   return Qnil;
 }
+
+#undef SET_PARAM_IF_SAME
 
 void
 init_ruby_whisper_context_params(VALUE *cContext)
@@ -112,17 +161,17 @@ init_ruby_whisper_context_params(VALUE *cContext)
   cContextParams = rb_define_class_under(*cContext, "Params", rb_cObject);
 
   rb_define_alloc_func(cContextParams, ruby_whisper_context_params_s_allocate);
-  rb_define_method(cContextParams, "initialize", ruby_whisper_context_params_initialize, 0);
-  rb_define_method(cContextParams, "use_gpu", ruby_whisper_context_params_get_use_gpu, 0);
-  rb_define_method(cContextParams, "use_gpu=", ruby_whisper_context_params_set_use_gpu, 1);
-  rb_define_method(cContextParams, "flash_attn", ruby_whisper_context_params_get_flash_attn, 0);
-  rb_define_method(cContextParams, "flash_attn=", ruby_whisper_context_params_set_flash_attn, 1);
-  rb_define_method(cContextParams, "gpu_device", ruby_whisper_context_params_get_gpu_device, 0);
-  rb_define_method(cContextParams, "gpu_device=", ruby_whisper_context_params_set_gpu_device, 1);
-  rb_define_method(cContextParams, "dtw_token_timestamps", ruby_whisper_context_params_get_dtw_token_timestamps, 0);
-  rb_define_method(cContextParams, "dtw_token_timestamps=", ruby_whisper_context_params_set_dtw_token_timestamps, 1);
-  rb_define_method(cContextParams, "dtw_aheads_preset", ruby_whisper_context_params_get_dtw_aheads_preset, 0);
-  rb_define_method(cContextParams, "dtw_aheads_preset=", ruby_whisper_context_params_set_dtw_aheads_preset, 1);
-  rb_define_method(cContextParams, "dtw_n_top", ruby_whisper_context_params_get_dtw_n_top, 0);
-  rb_define_method(cContextParams, "dtw_n_top=", ruby_whisper_context_params_set_dtw_n_top, 1);
+  rb_define_method(cContextParams, "initialize", ruby_whisper_context_params_initialize, -1);
+
+  DEFINE_PARAM(use_gpu, 0)
+  DEFINE_PARAM(flash_attn, 1)
+  DEFINE_PARAM(gpu_device, 2)
+  DEFINE_PARAM(dtw_token_timestamps, 3)
+  DEFINE_PARAM(dtw_aheads_preset, 4)
+  DEFINE_PARAM(dtw_n_top, 5)
 }
+
+#undef DEFINE_PARAM
+#undef DEF_INT_ATTR_METHOD
+#undef DEF_BOOLEAN_ATTR_METHOD
+#undef NUM_PARAMS
