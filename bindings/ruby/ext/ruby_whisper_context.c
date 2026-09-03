5443ebdbeb432fee50ff8af59ad755d7e2c8bd0d
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2026-09-03 23:13:58 +0900

    Check MemoryView format more strictly

--- bindings/ruby/ext/ruby_whisper_context.c
+++ bindings/ruby/ext/ruby_whisper_context.c
@@ -316,21 +316,47 @@ VALUE ruby_whisper_model_type(VALUE self)
 static bool
 check_memory_view(rb_memory_view_t *memview)
 {
+  rb_memory_view_prepare_item_desc(memview);
+
   if (!memview->format) {
-    rb_warn("currently format is required");
+    rb_warn("format is required");
     return false;
   }
 
-  if (strcmp(memview->format, "f") == 0) {
-    // accept
-  } else if (strcmp(memview->format, "e") == 0) {
-    if (IS_BIGENDIAN) {
-      rb_warn("currently format \"e\" is only supported on little-endian environment");
-      return false;
-    }
-  } else {
-    rb_warn("currently only format \"f\" and \"e\" on little-endian environment is supported for MemoryView, but given: %s", memview->format);
+  if (memview->item_desc.length != 1) {
+    rb_warn("format must be exact one character");
     return false;
+  }
+
+  rb_memory_view_item_component_t component = memview->item_desc.components[0];
+
+  if (component.offset) {
+    rb_warn("format has offset");
+    return false;
+  }
+  if (component.repeat != 1) {
+    rb_warn("format repeated");
+    return false;
+  }
+  switch (component.format) {
+    case 'f':
+      // accept
+      break;
+    case 'e':
+      if (IS_BIGENDIAN) {
+        rb_warn("currently format \"e\" is only supported on little-endian environment");
+        return false;
+      }
+      break;
+    case 'g':
+      if (!IS_BIGENDIAN) {
+        rb_warn("currently format \"g\" is only supported on big-endian environment");
+        return false;
+      }
+      break;
+    default:
+      rb_warn("currently only format \"f\", \"e\" on little-endian environment and \"g\" on big-endian environment are supported for MemoryView, but given: %c", component.format);
+      return false;
   }
 
   if (memview->ndim != 1 && !(memview->ndim == 2 && memview->shape[1] == 1)) {
